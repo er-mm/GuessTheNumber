c19f8e843d74865a2994b6a07415eb59f6c8820a
Mayank Mittal <mayank.mittal@allocatesoftware.com>
2020-09-09 03:17:11 +0530

    Updates

---
+++ precache-manifest.09bb240473907a686d986228fae9ddc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f42a6b71c3cef9f4746dcac7d71483b2",
+    "url": "/GuessTheNumber/index.html"
+  },
+  {
+    "revision": "49fad8dd8c36432fc9fa",
+    "url": "/GuessTheNumber/static/js/2.411b99fc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/GuessTheNumber/static/js/2.411b99fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "407e5f21a61b5dfe0402",
+    "url": "/GuessTheNumber/static/js/main.c5fd49af.chunk.js"
+  },
+  {
+    "revision": "db01671bb3dec93c0198",
+    "url": "/GuessTheNumber/static/js/runtime-main.cf45892a.js"
+  },
+  {
+    "revision": "0cecf8c02cc3ed001fd70962cba19da8",
+    "url": "/GuessTheNumber/static/media/bg.0cecf8c0.jpg"
+  }
+]);
