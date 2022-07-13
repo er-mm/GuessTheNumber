30275a357149b7066634f2c6d585b6a555818f7a
er-mmm10 <ermm10051995@gmail.com>
2022-07-13 10:28:59 +0530

    compiled warnings

--- guess-the-number/src/App.js
+++ guess-the-number/src/App.js
@@ -1,43 +1,41 @@
-import React, { useState } from 'react';
-import Instructions from './Instructions';
-import Game from './Game';
-import EnterName from './EnterName';
-import { ThemeProvider } from '@material-ui/styles';
-import theme from './Theme';
-import { makeStyles } from '@material-ui/core/styles';
-import bg from './bg.jpg';
+import React, { useState } from "react";
+import Instructions from "./Instructions";
+import Game from "./Game";
+import EnterName from "./EnterName";
+import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles(theme => ({
-	main: {
-		backgroundImage: `url(./bg.jpg)`,
-		backgroundPosition: 'center',
-		backgroundSize: 'cover',
-		backgroundRepeat: 'no-repeat',
-		height: '100%',
-		width: '100%',
-		position: 'fixed'
-	},
-	sub: {
-		margin: 20
-	}
+const useStyles = makeStyles((theme) => ({
+  main: {
+    backgroundImage: `url(./bg.jpg)`,
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    height: "100%",
+    width: "100%",
+    position: "fixed",
+  },
+  sub: {
+    margin: 20,
+  },
 }));
 
 export default () => {
-	const classes = useStyles();
-	const [value, setValue] = useState(false);
-	const [name, setName] = useState('');
-	return (
-	<>
-		<div className={classes.main}>
-			<div className={classes.sub}>	
-		{!name
-			? <EnterName setName={setName} />
-			: !value
-				? <Instructions setValue={setValue} name={name} />
-				: <Game setValue={setValue} setName={setName} name={name}/>
-		}
-		</div>
-		</div>
-		</>
-	);
+  const classes = useStyles();
+  const [value, setValue] = useState(false);
+  const [name, setName] = useState("");
+  return (
+    <>
+      <div className={classes.main}>
+        <div className={classes.sub}>
+          {!name ? (
+            <EnterName setName={setName} />
+          ) : !value ? (
+            <Instructions setValue={setValue} name={name} />
+          ) : (
+            <Game setValue={setValue} setName={setName} name={name} />
+          )}
+        </div>
+      </div>
+    </>
+  );
 };
